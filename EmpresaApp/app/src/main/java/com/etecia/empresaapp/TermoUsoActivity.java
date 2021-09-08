01e425d1fe87d64ccaf3febc1ca7f4125012786c
alanferrari <alanferrari33@gmail.com>
2021-09-08 20:46:59 -0300

    projeto empresa

---
+++ EmpresaApp/app/src/main/java/com/etecia/empresaapp/TermoUsoActivity.java
@@ -0,0 +1,14 @@
+package com.etecia.empresaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermoUsoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termo_uso);
+    }
+}
